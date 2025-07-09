b4f5e7a340acd377ebe3e6da1e82a73eb2d2b022
kris <thientan.3003.01@gmail.com>
2025-07-09 17:39:24 +0700

    ngay2

---
+++ src/app/kris/bt1.js
@@ -0,0 +1,8 @@
+let a = 15
+function tinhDienTichHinhTron() {
+    const pi = 3.14;
+    let bankinh = a;
+    var dientich = pi * bankinh * bankinh;
+    console.log(`Diện tích hình tròn có bán kính ${bankinh}  là ${dientich} `)
+};
+tinhDienTichHinhTron();
---
+++ src/app/kris/ltfunction.js
@@ -0,0 +1,7 @@
+function dientich(a, b) { //gọi tên hàm
+    //let a = 6; //khai báo biến, có thể bỏ dòng này và tự thêm biến ở phần gọi hàm
+    //let b = 8;
+    console.log(`Diện tích hình chữ nhật bằng dài a: ${a} x rộng b: ${b} = ${a * b}`); // dòng này để khai báo hàm
+}
+dientich(); //dòng này để gọi hàm
+dientich(4, 5); //gọi hàm thêm biến riêng
