b3e0611e0de0d22fc597450efb59dc09eb520963
Kris <kris@MacBook-Pro-cua-Kris.local>
2025-07-14 17:31:31 +0700

    updatecuoingay

---
+++ src/app/kris/configureStore.ts
@@ -0,0 +1,9 @@
+// store.ts
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from './counterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer
+  }
+});
---
+++ src/app/kris/hehe.tsx
@@ -0,0 +1,15 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement } from './counterSlice';
+
+function Counter() {
+  const count = useSelector((state: any) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch(increment())}>+</button>
+    </div>
+  );
+}
